ba6c2e7d82d7347356301a9f6f7984dc881c9eef
John Keisker <jkeisker@me.com>
2016-03-21 14:25:40 -0500

    second commit.

---
+++ app/app.js
@@ -0,0 +1,48 @@
+// querySelector
+// document.querySelector("subtract");
+// document.querySelector("multiply");
+// document.querySelector("divide");
+
+document
+  .querySelector("#add")
+  .addEventListener("click", function() {
+    var firstNumber = document.querySelector("#numberOne").value;
+    var secondNumber = document.querySelector("#numberTwo").value;
+    // add numbers together
+    var total = Number(firstNumber) + Number(secondNumber);
+    // change answer to be the result
+    document.querySelector("#answer").innerHTML = total;
+  });
+
+  document
+    .querySelector("#subtract")
+    .addEventListener("click", function() {
+      var firstNumber = document.querySelector("#numberOne").value;
+      var secondNumber = document.querySelector("#numberTwo").value;
+      // add numbers together
+      var total = Number(firstNumber) - Number(secondNumber);
+      // change answer to be the result
+      document.querySelector("#answer").innerHTML = total;
+    });
+
+    document
+      .querySelector("#multiply")
+      .addEventListener("click", function() {
+        var firstNumber = document.querySelector("#numberOne").value;
+        var secondNumber = document.querySelector("#numberTwo").value;
+        // add numbers together
+        var total = Number(firstNumber) * Number(secondNumber);
+        // change answer to be the result
+        document.querySelector("#answer").innerHTML = total;
+      });
+
+      document
+        .querySelector("#divide")
+        .addEventListener("click", function() {
+          var firstNumber = document.querySelector("#numberOne").value;
+          var secondNumber = document.querySelector("#numberTwo").value;
+          // add numbers together
+          var total = Number(firstNumber) / Number(secondNumber);
+          // change answer to be the result
+          document.querySelector("#answer").innerHTML = total;
+        });
