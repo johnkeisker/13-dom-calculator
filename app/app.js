a3ade9150063125f5176309cb9ab6bf348ef356b
John Keisker <jkeisker@me.com>
2016-03-21 15:29:15 -0500

    commit 3.

--- app/app.js
+++ app/app.js
@@ -1,7 +1,4 @@
-// querySelector
-// document.querySelector("subtract");
-// document.querySelector("multiply");
-// document.querySelector("divide");
+
 
 document
   .querySelector("#add")
@@ -14,35 +11,35 @@ document
     document.querySelector("#answer").innerHTML = total;
   });
 
-  document
-    .querySelector("#subtract")
-    .addEventListener("click", function() {
-      var firstNumber = document.querySelector("#numberOne").value;
-      var secondNumber = document.querySelector("#numberTwo").value;
-      // add numbers together
-      var total = Number(firstNumber) - Number(secondNumber);
-      // change answer to be the result
-      document.querySelector("#answer").innerHTML = total;
-    });
+document
+  .querySelector("#subtract")
+  .addEventListener("click", function() {
+    var firstNumber = document.querySelector("#numberOne").value;
+    var secondNumber = document.querySelector("#numberTwo").value;
+    // add numbers together
+    var total = Number(firstNumber) - Number(secondNumber);
+    // change answer to be the result
+    document.querySelector("#answer").innerHTML = total;
+  });
 
-    document
-      .querySelector("#multiply")
-      .addEventListener("click", function() {
-        var firstNumber = document.querySelector("#numberOne").value;
-        var secondNumber = document.querySelector("#numberTwo").value;
-        // add numbers together
-        var total = Number(firstNumber) * Number(secondNumber);
-        // change answer to be the result
-        document.querySelector("#answer").innerHTML = total;
-      });
+document
+  .querySelector("#multiply")
+  .addEventListener("click", function() {
+    var firstNumber = document.querySelector("#numberOne").value;
+    var secondNumber = document.querySelector("#numberTwo").value;
+    // add numbers together
+    var total = Number(firstNumber) * Number(secondNumber);
+    // change answer to be the result
+    document.querySelector("#answer").innerHTML = total;
+  });
 
-      document
-        .querySelector("#divide")
-        .addEventListener("click", function() {
-          var firstNumber = document.querySelector("#numberOne").value;
-          var secondNumber = document.querySelector("#numberTwo").value;
-          // add numbers together
-          var total = Number(firstNumber) / Number(secondNumber);
-          // change answer to be the result
-          document.querySelector("#answer").innerHTML = total;
-        });
+document
+  .querySelector("#divide")
+  .addEventListener("click", function() {
+    var firstNumber = document.querySelector("#numberOne").value;
+    var secondNumber = document.querySelector("#numberTwo").value;
+    // add numbers together
+    var total = Number(firstNumber) / Number(secondNumber);
+    // change answer to be the result
+    document.querySelector("#answer").innerHTML = total;
+  });
